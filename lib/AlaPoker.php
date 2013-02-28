8f20fab0a48f93feba9a8963099ef3cfffb8ec03
Sashank Tadepalli <soulcyon@gmail.com>
2013-02-28 04:10:33 -0500

    Hopefully this shit dont conflict

--- lib/AlaPoker.php
+++ lib/AlaPoker.php
@@ -1,18 +1,18 @@
 <?php
-
+require_once("Tables.php");
 require_once("Hands.php");
-
+require_once("Yield.php");
 class AlaPoker {
-	
 	private $hands = array();
-	private $board = array();
-	private $deads = array();
+	private $board = 0;
+	private $deads = 0;
 	private $wins = array();
 	private $ties = array();
 	private $loss = array();
 	private $totalHands = 0;
 
 	public function __construct($hands, $board, $deads) {
+<<<<<<< HEAD
 		
 		$this->hands = Hands::parse($hands, true);
 		$this->board = Hands::parse($board);
@@ -25,7 +25,19 @@ class AlaPoker {
 		$deads_mask = Hands::mask_cards($deads);
 
 		var_dump($board_mask);
+=======
+		$this->board = Hands::parse($board);
+		$this->deads = Hands::parse($deads);
+		$this->hands = Hands::parse($hands, true);
+		for($i = 0;$i < count($this->hands); $i++){
+			$this->deads |= $this->hands[$i];
+		}
+	}
+
+	public function getOdds() {
+		var_dump(iterator_to_array(Yield::generate(intval($this->board), intval($this->deads), 5)));
+		return $this->hands . ":" . $this->deads;
+>>>>>>> Hopefully this shit dont conflict
 	}
 }
-
 ?>
