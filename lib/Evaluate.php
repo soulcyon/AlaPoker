a7953c626f692f0b5839a4c7b9c3e204813d5ad3
Sashank Tadepalli <soulcyon@gmail.com>
2013-02-28 17:05:16 -0500

    Completed connection between Evaluate and Tables

--- lib/Evaluate.php
+++ lib/Evaluate.php
@@ -1,101 +1,86 @@
 <?php
-
 class Evaluate {
-    
-    public static function evaluate7($cards = 3378524412361665, $numberOfCards = 7) {
-        
-        $retval = 0; 
-        $four_mask; 
-        $three_mask; 
+    // $cards = 3378524412361665
+    // $cards = 2181499788985344
+    public static function evaluate7($cards = 2181499788985344, $numberOfCards = 7) {
+        $retval = 0;
+        $four_mask;
+        $three_mask;
         $two_mask;
 
-        if ($numberOfCards < 1 || $numberOfCards > 7) 
-            throw new Exception("numberOfCards must be b/w 1 and 7");
-
-        $sc = ($cards >> (0)) & 0x1fff;
-        $sd = ($cards >> (1)) & 0x1fff;
-        $sh = ($cards >> (2)) & 0x1fff;
-        $ss = ($cards >> (3)) & 0x1fff;
+        $sc = ($cards >> 0) & 0x1fff;
+        $sd = ($cards >> 13) & 0x1fff;
+        $sh = ($cards >> 26) & 0x1fff;
+        $ss = ($cards >> 39) & 0x1fff;
 
         $ranks = $sc | $sd | $sh | $ss;
-        $n_ranks = $nBitsTable[$ranks];
+        $n_ranks = Tables::$N_BITS_TABLE[$ranks];
         $n_dups = ($numberOfCards - $n_ranks);
 
-        // Check for Straight, Flush, or Straight Flush
         if ($n_ranks >= 5)
         {
-            if ($nBitsTable[$ss] >= 5)
+            if (Tables::$N_BITS_TABLE[$ss] >= 5)
             {
-                if ($straightTable[$ss] != 0)
-                    return $HANDTYPE_VALUE_STRAIGHTFLUSH + ($straightTable[$ss] << $$TOP_CARD_SHIFT);
+                if (Tables::$STRAIGHT_TABLE[$ss] != 0)
+                    return 134217728 + (Tables::$STRAIGHT_TABLE[$ss] << 16);
                 else
-                    $retval = $HANDTYPE_VALUE_FLUSH + $$topFiveCardsTable[$ss];
+                    $retval = 83886080 + Tables::$TOP_FIVE_CARDS_TABLE[$ss];
             }
-            else if ($nBitsTable[$sc] >= 5)
+            else if (Tables::$N_BITS_TABLE[$sc] >= 5)
             {
-                if ($straightTable[$sc] != 0)
-                    return $HANDTYPE_VALUE_STRAIGHTFLUSH + ($straightTable[$sc] << $$TOP_CARD_SHIFT);
+                if (Tables::$STRAIGHT_TABLE[$sc] != 0)
+                    return 134217728 + (Tables::$STRAIGHT_TABLE[$sc] << 16);
                 else
-                    $retval = $HANDTYPE_VALUE_FLUSH + $$topFiveCardsTable[$sc];
+                    $retval = 83886080 + Tables::$TOP_FIVE_CARDS_TABLE[$sc];
             }
-            else if ($nBitsTable[$sd] >= 5)
+            else if (Tables::$N_BITS_TABLE[$sd] >= 5)
             {
-                if ($straightTable[$sd] != 0)
-                    return $HANDTYPE_VALUE_STRAIGHTFLUSH + ($straightTable[$sd] << $$TOP_CARD_SHIFT);
+                if (Tables::$STRAIGHT_TABLE[$sd] != 0)
+                    return 134217728 + (Tables::$STRAIGHT_TABLE[$sd] << 16);
                 else
-                    $retval = $HANDTYPE_VALUE_FLUSH + $$topFiveCardsTable[$sd];
+                    $retval = 83886080 + Tables::$TOP_FIVE_CARDS_TABLE[$sd];
             }
-            else if ($nBitsTable[$sh] >= 5)
+            else if (Tables::$N_BITS_TABLE[$sh] >= 5)
             {
-                if ($straightTable[$sh] != 0)
-                    return $HANDTYPE_VALUE_STRAIGHTFLUSH + ($straightTable[$sh] << $$TOP_CARD_SHIFT);
+                if (Tables::$STRAIGHT_TABLE[$sh] != 0)
+                    return 134217728 + (Tables::$STRAIGHT_TABLE[$sh] << 16);
                 else
-                    $retval = $HANDTYPE_VALUE_FLUSH + $$topFiveCardsTable[$sh];
+                    $retval = 83886080 + Tables::$TOP_FIVE_CARDS_TABLE[$sh];
             }
             else
             {
-                $st = $straightTable[$ranks];
+                $st = Tables::$STRAIGHT_TABLE[$ranks];
                 if ($st != 0)
-                    $retval = $HANDTYPE_VALUE_STRAIGHT + ($st << $$TOP_CARD_SHIFT);
+                    $retval = 67108864 + ($st << 16);
             };
 
-            // FH / Quads not possible, skips the precess of computing $two_mask / three_mask / etc.
             if ($retval != 0 && $n_dups < 3) 
                 return $retval;
         }
 
-        // Flush / Straight not possible anymore
-        // But FH / QH possible b/c of duplicates
-
         switch ($n_dups)
         {
             case 0:
-                // It's a no-pair hand
-                return $HANDTYPE_VALUE_HIGHCARD + $$topFiveCardsTable[$ranks];
+                return Tables::$TOP_FIVE_CARDS_TABLE[$ranks];
             case 1: {
-                    // It's a one-pair hand
                     $t; $kickers;
-
-                    $$two_mask = $ranks ^ ($sc ^ $sd ^ $sh ^ $ss);
-                    
-                    $retval = ($HANDTYPE_VALUE_PAIR + ($topCardTable[$$two_mask] << $$TOP_CARD_SHIFT));
-                    $t = $ranks ^ $$two_mask; 
-                    /* Only one bit set in $two_mask. Get the top five $cards in what is left, drop all 
-                    but the top three cards, and shift them by one to get the three desired kickers. */
-                    $kickers = ($$topFiveCardsTable[t] >> $CARD_WIDTH) & ~$FIFTH_CARD_MASK;
+                    $two_mask = $ranks ^ ($sc ^ $sd ^ $sh ^ $ss);
+                    $retval = (16777216 + (Tables::$TOP_CARDS_TABLE[$two_mask] << 16));
+                    $t = $ranks ^ $two_mask;
+                    $kickers = (Tables::$TOP_FIVE_CARDS_TABLE[t] >> 4) & -16;
                     $retval += $kickers;
                     return $retval;
                 }
 
-            case 2: /* Either two pair or trips */
+            case 2:
                 $two_mask = $ranks ^ ($sc ^ $sd ^ $sh ^ $ss);
-                if ($$two_mask != 0)
+                if ($two_mask != 0)
                 {
-                    $t = $ranks ^ $$two_mask; /* Exactly two bits set in $two_mask */
-                    $retval = ($HANDTYPE_VALUE_TWOPAIR 
-                        + ($topFiveCardsTable[$two_mask]
-                        & ($TOP_CARD_MASK | $SECOND_CARD_MASK))
-                        + ($topCardTable[t] << $THIRD_CARD_SHIFT));
+                    $t = $ranks ^ $two_mask;
+                    $retval = (33554432 
+                        + (Tables::$TOP_FIVE_CARDS_TABLE[$two_mask]
+                        & 1044480)
+                        + (Tables::$TOP_CARDS_TABLE[t] << $THIRD_CARD_SHIFT));
 
                     return $retval;
                 }
@@ -103,66 +88,53 @@ class Evaluate {
                 {
                     $t; $second;
                     $three_mask = (($sc & $sd) | ($sh & $ss)) & (($sc & $sh) | ($sd & $ss));
-                    $retval = ($HANDTYPE_VALUE_TRIPS + ($topCardTable[$three_mask] << $TOP_CARD_SHIFT));
+                    $retval = (50331648 + (Tables::$TOP_CARDS_TABLE[$three_mask] << 16));
                     $t = $ranks ^ $three_mask; /* Only one bit set in three_mask */
-                    $second = $topCardTable[t];
-                    $retval += ($second << $SECOND_CARD_SHIFT);
+                    $second = Tables::$TOP_CARDS_TABLE[t];
+                    $retval += ($second << 12);
                     $t ^= (1 << $second);
-                    $retval += ($topCardTable[t] << $THIRD_CARD_SHIFT);
+                    $retval += (Tables::$TOP_CARDS_TABLE[t] << $THIRD_CARD_SHIFT);
                     return $retval;
                 }
 
-                default:
-                    // Possible Quads, F H, Straight or Flush, or Two pair
-                    $four_mask = $sh & $sd & $sc & $ss;
-                    if ($four_mask != 0)
-                    {
-                        $tc = $topCardTable[$four_mask];
-                        $retval = ($HANDTYPE_VALUE_FOUR_OF_A_KIND
-                            + ($tc << $TOP_CARD_SHIFT)
-                            + (($topCardTable[$ranks ^ (1 << $tc)]) << $SECOND_CARD_SHIFT));
-                        return $retval;
-                    }
-
-                /* Technically, three_mask as defined below is really the set of
-                bits which are set in three or four of the suits, but since
-                we've already eliminated quads, this is OK
-                Similarly, $two_mask is really two_or_four_mask, but since we've
-                already eliminated quads, we can use this shortcut */
+            default:
+                $four_mask = $sh & $sd & $sc & $ss;
+                if ($four_mask != 0)
+                {
+                    $tc = Tables::$TOP_CARDS_TABLE[$four_mask];
+                    $retval = (117440512
+                        + ($tc << 16)
+                        + ((Tables::$TOP_CARDS_TABLE[$ranks ^ (1 << $tc)]) << 12));
+                    return $retval;
+                }
 
                 $two_mask = $ranks ^ ($sc ^ $sd ^ $sh ^ $ss);
-                if ($nBitsTable[$two_mask] != $n_dups)
+                if (Tables::$N_BITS_TABLE[$two_mask] != $n_dups)
                 {
-                    /* Must be some trips then, which really means there is a 
-                       full house since n_dups >= 3 */
                     $tc; $t;
                     $three_mask = (($sc & $sd) | ($sh & $ss)) & (($sc & $sh) | ($sd & $ss));
-                    $retval = $HANDTYPE_VALUE_FULLHOUSE;
-                    $tc = $topCardTable[$three_mask];
-                    $retval += ($tc << $TOP_CARD_SHIFT);
-                    $t = ($two_mask | $three_mask) ^ (1 << $$tc);
-                    $retval += ($topCardTable[$t] << $SECOND_CARD_SHIFT);
+                    $retval = 100663296;
+                    $tc = Tables::$TOP_CARDS_TABLE[$three_mask];
+                    $retval += ($tc << 16);
+                    $t = ($two_mask | $three_mask) ^ (1 << $tc);
+                    $retval += (Tables::$TOP_CARDS_TABLE[$t] << 12);
                     return $retval;
                 }
 
-                if ($retval != 0) /* Flush and Straight */
+                if ($retval != 0)
                     return $retval;
                 else
                 {
-                    /* Must be two pair */
                     $top; $second;
-
-                    $retval = $HANDTYPE_VALUE_TWOPAIR;
-                    $top = $topCardTable[$two_mask];
-                    $retval += ($top << $TOP_CARD_SHIFT);
-                    $second = $topCardTable[$two_mask ^ (1 << $top)];
-                    $retval += ($second << $SECOND_CARD_SHIFT);
-                    $retval += (($topCardTable[$ranks ^ (1 << $top) ^ (1 << $second)]) << $THIRD_CARD_SHIFT);
+                    $retval = 33554432;
+                    $top = Tables::$TOP_CARDS_TABLE[$two_mask];
+                    $retval += ($top << 16);
+                    $second = Tables::$TOP_CARDS_TABLE[$two_mask ^ (1 << $top)];
+                    $retval += ($second << 12);
+                    $retval += ((Tables::$TOP_CARDS_TABLE[$ranks ^ (1 << $top) ^ (1 << $second)]) << $THIRD_CARD_SHIFT);
                     return $retval;
                 }
         }
     }
 }
-
-Evaluate::evaluate7();
 ?>
