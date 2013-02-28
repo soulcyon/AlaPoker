e0e32a9d1fe110faebb54176a7493c2321ed2e1c
Sashank Tadepalli <soulcyon@gmail.com>
2013-02-28 04:15:25 -0500

    Merge complete

--- lib/Hands.php
+++ lib/Hands.php
@@ -1,21 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-class Hands {
-	
-	private static $suits = array("C" => 0, "D" => 1, "H" => 2, "S" => 3);
-	private static $ranks = array("2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4, "7" => 5, "8" => 6, "9" => 7, 
-		"T" => 8, "J" => 9, "Q" => 10, "K" => 11, "A" => 12);
-
-	public static function parse($cards_str, $is_pockets = false) {
-		if(strlen($cards_str) == 0)
-			return false;
-
-		if($is_pockets === false) {
-			$temp = explode(" ", $cards_str);
-			Hands::mask_cards($temp);
-			return $temp;
-=======
 class Hands {
 	private static $suits = array("C" => 0, "D" => 1, "H" => 2, "S" => 3);
 	private static $ranks = array("2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4, "7" => 5, "8" => 6, "9" => 7, "T" => 8, "J" => 9, "Q" => 10, "K" => 11, "A" => 12);
@@ -26,29 +9,11 @@ class Hands {
 
 		if( $is_pockets === false ){
 			return self::mask_cards(explode(" ", $cards_str));
->>>>>>> Hopefully this shit dont conflict
 		}
 
 		$temp_cards = explode(" ", $cards_str);
 		$cards = array();
-<<<<<<< HEAD
-		if(count($temp_cards) % 2 == 1) {
-			array_pop($temp_cards);
-		}
-		for($i = 0; $i < count($temp_cards); $i+=2) {
-			$cards[$i/2] = array($temp_cards[$i], $temp_cards[$i + 1]);
-			Hands::mask_cards($cards[$i/2]);
-		}
-		return $cards;
-	}
-
-	public static function mask_cards($cards){
-		$hand_mask = 0;
-		for($i = 0; $i < count($cards); $i++) {
-			$card = Hands::$ranks[$cards[$i]{0}] + 0xD * Hands::$suits[$cards[$i]{1}];
-			var_dump($cards[$i] . "_" . $card . ":" . $hand_mask . ":" . 1);
-=======
-		if( count($temp_cards) % 2 == 1 ){
+		if (count($temp_cards) % 2 == 1) {
 			array_pop($temp_cards);
 		}
 		for($i = 0; $i < count($temp_cards); $i+=2){
@@ -63,7 +28,6 @@ class Hands {
 		$hand_mask = 0;
 		for($i = 0; $i < count($cards); $i++) {
 			$card = self::$ranks[$cards[$i]{0}] + 0xD * self::$suits[$cards[$i]{1}];
->>>>>>> Hopefully this shit dont conflict
 			$hand_mask |= 1 << $card;
 		}
 		return $hand_mask;
